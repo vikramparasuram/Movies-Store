0343ab6ca847776ff6acd34c5718c77134d92b71
Vikram Parasuram <vikramparasuram@gmail.com>
2025-09-16 16:03:26 +0000

    Edits made on PythonAnywhere

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c4v$cvvsq5(i&wij7+gn#9b&-_4m*$)o@nq)1fzfp6bw0r&bby
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,18 +122,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'moviesstore' / 'static']
 
+# For deployment: collectstatic will put everything here
+STATIC_ROOT = BASE_DIR / 'static'
+
+# Media files (user uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 LOGIN_REDIRECT_URL = reverse_lazy('home:index')
 LOGOUT_REDIRECT_URL = reverse_lazy('home:index')
